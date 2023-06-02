3f8a88bb67a37980e919910767a75ad59c19dcab
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2023-06-02 15:36:30 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Ryan Martin. and affiliates.
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import "./App.css";
 
 function App() {
