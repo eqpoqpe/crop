ec0d79171ee50a46aa9461cce24b355b02c10278
eqpoqpe <eqpoqpe@gmail.com>
2023-06-07 17:06:46 +0800

    upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/app",
   build: {
-    manifest: true
+    manifest: true,
+    minify: "terser"
   }
 });
